9ea105e071e2577578df3b9c063cc47e838de90a
Ojas Inamdar <ojasinamdar101@gmail.com>
2021-10-01 13:05:29 +0530

    Added highscore functionality and reset highscore feature

--- snake.js
+++ snake.js
@@ -1,14 +1,24 @@
-
-
-
-	// this Function will run only once
-function init(){
+// this Function will run only once
+function init() {
 	canvas = document.getElementById('mycanvas');
 	W = H = canvas.width = canvas.height = 1000;
 	pen = canvas.getContext('2d');
 	cs = 66;
 	game_over = false;
 	score = 5;
+
+	//Adds listener to reset high score
+	resetButton = document.getElementById('reset-high-score-btn');
+	resetButton.addEventListener('click', () => {
+		localStorage.removeItem('highScore');
+		high_score = localStorage.getItem('highScore') || 5;
+		highScoreText.innerText = `High score: ${high_score}`;
+	}, { once: true });
+
+	//Get highscore from localstorage and update display
+	highScoreText = document.getElementById('high-score');
+	high_score = localStorage.getItem('highScore') || 5;
+	highScoreText.innerText = `High score: ${high_score}`;
 
 
 	//Create a Image Object for food
@@ -21,70 +31,69 @@ function init(){
 	food = getRandomFood(); // getting random cordinates for food
 
 	snake = {
-		init_len:5,
-		color:"blue",
-		cells:[],
-		direction:"right",
+		init_len: 5,
+		color: "blue",
+		cells: [],
+		direction: "right",
 
 
-		createSnake:function(){
-			for(var i=this.init_len;i>0;i--){
-				this.cells.push({x:i,y:0});
+		createSnake: function () {
+			for (var i = this.init_len; i > 0; i--) {
+				this.cells.push({ x: i, y: 0 });
 			}
 		},
-		drawSnake:function(){
+		drawSnake: function () {
 
-			for(var i=0;i<this.cells.length;i++){
+			for (var i = 0; i < this.cells.length; i++) {
 				pen.fillStyle = this.color;
-				pen.fillRect(this.cells[i].x*cs,this.cells[i].y*cs,cs-3,cs-3);
+				pen.fillRect(this.cells[i].x * cs, this.cells[i].y * cs, cs - 3, cs - 3);
 			}
 		},
 
-		updateSnake:function(){
-							//console.log("updating snake according to the direction property");
-							//check if the snake has eaten food, increase the length of the snake and 
-							//generate new food object
+		updateSnake: function () {
+			//console.log("updating snake according to the direction property");
+			//check if the snake has eaten food, increase the length of the snake and 
+			//generate new food object
 			var headX = this.cells[0].x;
 			var headY = this.cells[0].y;
 
-			if(headX==food.x && headY==food.y){
+			if (headX == food.x && headY == food.y) {
 				console.log("Food eaten");
 				food = getRandomFood();
 				score++;
 
 			}
-			else
-			{
+			else {
 				this.cells.pop();
 			}
 
-			
-			var nextX,nextY;
 
-			if(this.direction=="right"){
+			var nextX, nextY;
+
+			if (this.direction == "right") {
 				nextX = headX + 1;
 				nextY = headY;
 			}
-			else if(this.direction=="left"){
+			else if (this.direction == "left") {
 				nextX = headX - 1;
 				nextY = headY;
 			}
-			else if(this.direction=="down"){
+			else if (this.direction == "down") {
 				nextX = headX;
 				nextY = headY + 1;
 			}
-			else{
+			else {
 				nextX = headX;
 				nextY = headY - 1;
 			}
 
-			this.cells.unshift({x: nextX,y:nextY});
+			this.cells.unshift({ x: nextX, y: nextY });
 
-						/*Logic that prevents snake from going out*/
-			var last_x = Math.round(W/cs);
-			var last_y = Math.round(H/cs);
+			/*Logic that prevents snake from going out*/
+			var last_x = Math.round(W / cs);
+			var last_y = Math.round(H / cs);
 
-			if(this.cells[0].y<0 || this.cells[0].x < 0 || this.cells[0].x > last_x || this.cells[0].y > last_y){
+			if (this.cells[0].y < 0 || this.cells[0].x < 0 || this.cells[0].x > last_x || this.cells[0].y > last_y) {
 				game_over = true;
 			}
 
@@ -93,80 +102,85 @@ function init(){
 	};
 
 	snake.createSnake();
-					//Add a Event Listener on the Document Object
-	function keyPressed(e){			// This will be returning some extra things but we only need key to access which key is pressed by a user
-					//Conditional Statments
-		if(e.key=="ArrowRight"){
+	//Add a Event Listener on the Document Object
+	function keyPressed(e) {			// This will be returning some extra things but we only need key to access which key is pressed by a user
+		//Conditional Statments
+		if (e.key == "ArrowRight") {
 			snake.direction = "right";		//for right key
 		}
-		else if(e.key=="ArrowLeft"){
+		else if (e.key == "ArrowLeft") {
 			snake.direction = "left";		// for left key
 		}
-		else if(e.key=="ArrowDown"){		
+		else if (e.key == "ArrowDown") {
 			snake.direction = "down";		// for down key
 		}
-		else{
+		else {
 			snake.direction = "up";			// for up key
 		}
 		console.log(snake.direction);
 	}
 
 
-	document.addEventListener('keydown',keyPressed) ;
-	
+	document.addEventListener('keydown', keyPressed);
+
 }
 
 
-function draw(){
-						//console.log("In Draw");
+function draw() {
+	//console.log("In Draw");
 
-						//erasing the old frame
-	pen.clearRect(0,0,W,H);
+	//erasing the old frame
+	pen.clearRect(0, 0, W, H);
 	snake.drawSnake();
 
 	pen.fillStyle = food.color;
-	pen.drawImage(food_img,food.x*cs,food.y*cs,cs,cs);
+	pen.drawImage(food_img, food.x * cs, food.y * cs, cs, cs);
 
-	pen.drawImage(trophy,18,20,cs,cs);
+	pen.drawImage(trophy, 18, 20, cs, cs);
 	pen.fillStyle = "blue";
 	pen.font = "20px Roboto"
-	pen.fillText(score,50,50);
+	pen.fillText(score, 50, 50);
 
-	
+
 }
 
-function update(){
-				//console.log("In Update");
-	snake.updateSnake(); 
+function update() {
+	//console.log("In Update");
+	snake.updateSnake();
 }
 
-function getRandomFood(){
+function getRandomFood() {
 
-	var foodX = Math.round(Math.random()*(W-cs)/cs); 		//getting random food value's X cordinate 
-	var foodY = Math.round(Math.random()*(H-cs)/cs);		// now I'm getting random value of Y cordinate 
+	var foodX = Math.round(Math.random() * (W - cs) / cs); 		//getting random food value's X cordinate 
+	var foodY = Math.round(Math.random() * (H - cs) / cs);		// now I'm getting random value of Y cordinate 
 
 	var food = {
-		x:foodX,
-		y:foodY,
-		color:"red",			
-	}	
+		x: foodX,
+		y: foodY,
+		color: "red",
+	}
 	return food;				// returning food with random food values(i.e, cordinates)
 
 }
 
-function gameloop(){					// will run until game is not over
-	if(game_over==true){
-		clearInterval(f);	
+function gameloop() {					// will run until game is not over
+	if (game_over == true) {
+		if (score > high_score) {
+			high_score = score;
+		}
+		highScoreText.innerText = `High score: ${high_score}`;  // Updates high score
+		localStorage.setItem('highScore', high_score); // Sets high score in LS
+		clearInterval(f);
 		alert("Game Over");			// popup for game over when it touches the boundary
 		return;
 	}
 	draw();
 	update();
 }
-		
+
 init();					// that top init function is called at first once
 
-var f = setInterval(gameloop,100); 		// setting interval of 100 milisecond
+var f = setInterval(gameloop, 100); 		// setting interval of 100 milisecond
 
 
 
